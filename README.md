9e0a8a14848fa0ee5c25213ebaab866d0a702216
Gwen <gwendall.esnault@gmail.com>
2015-02-25 23:10:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Meteor Accounts AngelList
+=========================
+
+Allow users to connect via their AngelList account.  
+
+Installation
+------------
+
+``` sh
+meteor add gwendall:auth-client-callbacks
+```
---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  name: "gwendall:accounts-angellist",
+  summary: "Login service for AngelList accounts",
+  version: "0.1.0",
+  git: "https://github.com/gwendall/meteor-accounts-angellist"
+});
+
+Package.onUse(function(api) {
+
+  api.use("accounts-base", ["client", "server"]);
+  api.imply("accounts-base", ["client", "server"]);
+  api.use("accounts-oauth", ["client", "server"]);
+
+  api.use("oauth", ["client", "server"]);
+  api.use("oauth2", ["client", "server"]);
+  api.use("http", ["server"]);
+  api.use("underscore", "server");
+  api.use("templating", "client");
+  api.use("random", "client");
+  api.use("service-configuration", ["client", "server"]);
+
+  api.addFiles("lib/accounts.js");
+  api.addFiles("lib/client.js", "client");
+  api.addFiles("lib/server.js", "server");
+
+});
